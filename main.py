26bd155496217461ae85e62281db159097a14ef8
Edward Minnett <ed@methodic.io>
2019-03-16 18:04:50 +0000

    Add first pass of the FCN logic

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -34,7 +32,16 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,8 +54,17 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+    l_3_size = vgg_layer3_out.get_shape().as_list()[-1]
+    l_4_size = vgg_layer4_out.get_shape().as_list()[-1]
+
+    fcn8 = tf.layers.conv2d(vgg_layer7_out, filters=num_classes, kernel_size=1, name="fcn8")
+    fcn9 = tf.layers.conv2d_transpose(fcn8, filters=l_4_size, kernel_size=4, strides=(2, 2), padding='SAME', name="fcn9")
+    fcn9_skip = tf.add(fcn9, vgg_layer4_out, name="fcn9_plus_vgg_layer4")
+    fcn10 = tf.layers.conv2d_transpose(fcn9_skip, filters=l_3_size, kernel_size=4, strides=(2, 2), padding='SAME', name="fcn10_conv2d")
+    fcn10_skip = tf.add(fcn10, vgg_layer3_out, name="fcn10_plus_vgg_layer3")
+    fcn11 = tf.layers.conv2d_transpose(fcn10_skip, filters=num_classes, kernel_size=16, strides=(8, 8), padding='SAME', name="fcn11")
+
+    return fcn11
 tests.test_layers(layers)
 
 
@@ -61,8 +77,14 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes), name="fcn_logits")
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
+
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label[:])
+    loss_op = tf.reduce_mean(cross_entropy, name="fcn_loss")
+    train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss_op, name="fcn_train_op")
+
+    return logits, train_op, loss_op
 tests.test_optimize(optimize)
 
 
@@ -81,8 +103,18 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+    
+    for epoch in range(epochs):
+        # Create function to get batches
+        total_loss = 0
+        for X_batch, gt_batch in get_batches_fn(batch_size):
+            feed_dict = {input_image: X_batch, correct_label: gt_batch, keep_prob: 0.5, learning_rate: 0.01}
+            loss, _ = sess.run([cross_entropy_loss, train_op], feed_dict=feed_dict)
+            total_loss += loss
+
+        print("EPOCH {} ...".format(epoch + 1))
+        print("Loss = {:.3f}".format(total_loss))
+        print()
 tests.test_train_nn(train_nn)
 
 
@@ -100,6 +132,12 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+    learning_rate = tf.placeholder(tf.float32)
+    correct_label = tf.placeholder(tf.float32, [None, None, None, num_classes])
+    
+    num_epochs = 30
+    batch_size = 64
+    
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
@@ -109,12 +147,22 @@ def run():
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
+        print("Build NN")
         # TODO: Build NN using load_vgg, layers, and optimize function
+        input_image, keep_prob, layer3, layer4, layer7 = load_vgg(sess, vgg_path)
+        model_output = layers(layer3, layer4, layer7, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(model_output, correct_label, learning_rate, num_classes)
 
+        print("Initialise Variables")
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+
+        print("Train NN")
         # TODO: Train NN using the train_nn function
+        train_nn(sess, num_epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
 
